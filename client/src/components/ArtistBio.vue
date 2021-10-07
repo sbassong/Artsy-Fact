664d007171d14171e9510757aa339951f4198a15
Sam Bassong <sam.bassong@gmail.com>
2021-10-07 09:57:20 -0700

    set up ArtistBio component

---
+++ client/src/components/ArtistBio.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="bio">
+    <div class='picture-cont'>
+      <img :src="picture" alt="">
+    </div>
+
+    <div class=bio-cont>
+      <button @click='goHome'>Home</button>
+      <h3>{{name}}</h3>
+      <h5>Bio: {{bio}}</h5>
+    </div>
+
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: 'ArtistDetails',
+  props: {
+    name: String,
+    picture: String,
+    bio: String
+  },
+  methods: {
+    goHome() {
+      this.$router.push('/')
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
