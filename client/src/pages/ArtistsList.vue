f808cf0a989ad640e637c0f174be2e2467d4c3d1
Sam Bassong <sam.bassong@gmail.com>
2021-10-07 09:30:02 -0700

    set up ArtistsList page

---
+++ client/src/pages/ArtistsList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+
+    <div class="artists" v-if='searched' hidden>
+      <h2>Artists</h2>
+      <section class="container-grid" v-for='artist in artists' :key='artist.id'>
+        <div @click="selectArtist(artist.id)">
+          <ArtistCard :picture='artist.picture' :name='artist.name' />
+        </div>
+      </section>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ArtistList',
+  components: {
+    ArtistCard
+  },
+  data: () => ({
+    artists: []
+  }),
+  mounted() {
+
+  },
+  methods: {
+    async getArtists() {
+      
+    }
+  }
+}
+</script>
