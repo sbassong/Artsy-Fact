74946f3b290903a1b7a440581d33e759dbb0d871
Sam Bassong <sam.bassong@gmail.com>
2021-10-07 10:30:58 -0700

    set up base PostForm component

---
+++ client/src/components/Forum.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="forum">
+
+    <section class="post-form-cont">
+      <button class='show-form-button' @click='renderForm'></button>
+      <PostForm v-if='clicked'/>
+    </section>
+
+    <section class="posts">
+        <h3>Posts</h3>
+      <div>
+        <section v-for='post in posts' :key='post.id'>
+          <div class="post">
+            <h4>Name: {{post.name}}</h4>
+            <h6>{{post.content}}</h6>
+          </div>
+        </section>
+      </div>
+      
+      
+    </section>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Forum',
+  data: () => ({
+    posts: []
+  }),
+  components: {
+    PostForm
+  },
+  props: {
+    artist_id: Number
+  },
+  mounted() {
+    this.getPosts()
+  },
+  methods: {
+    async getPosts() {
+      
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .post {
+    /* background-color: rgb(100, 97, 97); */
+    padding: .5em 1em;
+    margin: 0.5em auto;
+    border-radius: 20px
+  }
+
+  .post img,h6 {
+    display: inline-block;
+  }
+
+  .img-cont {
+    display: flex;
+  }
+
+  .img-cont div {
+    margin: 1em ;
+  }
+</style>
---
+++ client/src/components/PostForm.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+
+      <form @submit='handleSubmit'>
+        <input type="text" name='name' :value='reviewer' @input='handleChange'>
+        <input type="text" name='content' :value='content' @input='handleChange'>
+        <button>Post</button>
+      </form>
+      
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+  },
+  data: () => ({
+    reviewer: '',
+    content: '',
+
+  }),
+  methods: {
+    handleChange(event) {
+      this[event.target.name] = event.target.value
+    },
+    handleSubmit(event) {
+      event.preventDefault()
+      //use the post to posts table service here.
+    }
+  }
+}
+</script>
